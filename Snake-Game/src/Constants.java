fe97279c7ae4d49e851cc01a90b5e55bb6f75b52
chand-1907 <chandru190703@gmail.com>
2024-07-06 22:15:05 +0530

    Add four new constants to restrict and validate the grid creation

--- Snake-Game/src/Constants.java
+++ Snake-Game/src/Constants.java
@@ -1,10 +1,16 @@
 public class Constants {
 
 //    the minimum foods
-    public static int MIN_FOOD_COUNT = 3;
+    public static final int MIN_FOOD_COUNT = 1;
 
 //    the maximum foods
-    public static int MAX_FOOD_COUNT = 7;
+    public static final int MAX_FOOD_COUNT = 7;
 
+    public static final int MIN_ROW_COUNT = 2;
 
+    public static final int MAX_ROW_COUNT = 10;
+
+    public static final int MIN_COL_COUNT = 2;
+
+    public static final int MAX_COL_COUNT = 10;
 }
