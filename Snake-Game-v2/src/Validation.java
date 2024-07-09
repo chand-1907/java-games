06d4aa7d6d6314031e1d105d1634443b5295f579
chand-1907 <chandru190703@gmail.com>
2024-07-09 21:08:12 +0530

    normal commit

---
+++ Snake-Game-v2/src/Validation.java
@@ -0,0 +1,12 @@
+public interface Validation {
+
+    final class InputValidator {
+        public static boolean foodCountValidate (int count) {
+            return count >= Constants.MIN_FOOD_COUNT && count <= Constants.MAX_FOOD_COUNT;
+        }
+
+        public static boolean rowColumnValidator (int row, int col) {
+            return ((row <= Constants.MAX_ROW_COUNT && row >= Constants.MIN_ROW_COUNT) && (col <= Constants.MAX_COL_COUNT && col >= Constants.MIN_COL_COUNT));
+        }
+    }
+}
