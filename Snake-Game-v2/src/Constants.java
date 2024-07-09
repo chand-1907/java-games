8c0a4cddf95b5d0c9da24ed92b61e56f7cdebd77
chand-1907 <chandru190703@gmail.com>
2024-07-09 21:02:48 +0530

    Add a extra flexibility to the column cout

---
+++ Snake-Game-v2/src/Constants.java
@@ -0,0 +1,16 @@
+public class Constants {
+
+//    the minimum foods
+    public static final int MIN_FOOD_COUNT = 1;
+
+//    the maximum foods
+    public static final int MAX_FOOD_COUNT = 7;
+
+    public static final int MIN_ROW_COUNT = 2;
+
+    public static final int MAX_ROW_COUNT = 10;
+
+    public static final int MIN_COL_COUNT = 2;
+
+    public static final int MAX_COL_COUNT = 20;
+}
